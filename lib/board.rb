effb912ca6b803531fb5d1cd44d8e7fd550e0929
Edoardo Rota <edoardorota96@gmail.com>
2018-12-15 16:36:53 +0100

    Add a function to print the board on the console

---
+++ lib/board.rb
@@ -0,0 +1,26 @@
+class Board
+  require_relative "piece"
+
+  def initialize
+    @squares = Array.new(8, nil)
+    8.times { |i| @squares[i] = Array.new(8, nil) }
+  end
+
+  def print
+    puts "  +―――+―――+―――+―――+―――+―――+―――+―――+"
+    @squares.each_with_index do |row, index|
+      temp = []
+      row.each do |sqr|
+        if sqr.nil?
+          temp << "   "
+        else
+          temp << " " + sqr.img + " "
+        end
+      end
+      puts "#{8 - index} │" + temp.join("│") + "│"
+      puts "  +―――+―――+―――+―――+―――+―――+―――+―――+"
+    end
+    puts "    " + ("a".."h").to_a.join("   ")
+  end
+
+end
