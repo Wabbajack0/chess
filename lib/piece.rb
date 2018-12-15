2c54acaec40f69e5a0330c3c6b400339a4d47409
Edoardo Rota <edoardorota96@gmail.com>
2018-12-15 17:45:25 +0100

    Fix a bug that displayed the wrong piece name

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,38 +1,50 @@
 class Piece
   attr_accessor :is_white
 
-  @@img = nil
-
   def initialize(is_white = true)
     @is_black = is_white
-  end
-
-  def img
-    @@img
   end
 
 end
 
 class Pawn < Piece
   @@img = "P"
+  def img
+    @@img
+  end
 end
 
 class King < Piece
   @@img = "K"
+  def img
+    @@img
+  end
 end
 
 class Queen < Piece
   @@img = "Q"
+  def img
+    @@img
+  end
 end
 
 class Bishop < Piece
   @@img = "B"
+  def img
+    @@img
+  end
 end
 
 class Knight < Piece
   @@img = "N"
+  def img
+    @@img
+  end
 end
 
 class Rook < Piece
   @@img = "R"
+  def img
+    @@img
+  end
 end
