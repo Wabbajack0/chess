1b42e6a1922eb308ba9cab9c0ca47b87c79ee530
Edoardo Rota <edoardorota96@gmail.com>
2018-12-18 21:03:20 +0100

    Add the move function for pawns and kings and fix the color of the pieces to be correct on the console with black background)

--- lib/chess.rb
+++ lib/chess.rb
@@ -6,9 +6,39 @@ class Chess
     8.times { |i| @board[i] = Array.new(8, nil) }
   end
 
+  def move(x1, y1, x2, y2)
+    return false if x2 < 0 || y2 < 0
+    return false if x2 > 7 || y2 > 7
+    return false if x1 === x2 && y1 === y2
+
+    piece = @board[y1][x1]
+
+    return false if piece.nil?
+    return false if !@board[y2][x2].nil? && piece.is_white === @board[y2][x2].is_white
+    return false if !piece.correct_move?(x1, y1, x2, y2)
+
+    case
+    when piece.instance_of?(Pawn)
+      return false if (y2 - y1) === 2 && !@board[y1 + 1][x1].nil?
+      return false if (y2 - y1) === -2 && !@board[y1 - 1][x1].nil?
+      return false if x1 === x2 && !@board[y2][x2].nil?
+      return false if x1 != x2 && @board[y2][x2].nil?
+      @board[y1][x1] = nil
+      @board[y2][x2] = piece
+
+    when piece.instance_of?(King)
+      @board[y1][x1] = nil
+      @board[y2][x2] = piece
+    else
+      puts "what???"
+    end
+
+    return true
+  end
+
   def print
     puts "  +---+---+---+---+---+---+---+---+"
-    @board.each_with_index do |row, index|
+    @board.reverse.each_with_index do |row, index|
       temp = []
       row.each do |sqr|
         if sqr.nil?
--- lib/piece.rb
+++ lib/piece.rb
@@ -11,41 +11,83 @@ end
 class Pawn < Piece
   def initialize(is_white = true)
     super
-    is_white ? @img = "♙" : @img = "♟"
+    @is_white ? @img = "♟" : @img = "♙"
+  end
+
+  def correct_move?(x1, y1, x2, y2)
+    return false if (x2 - x1).abs > 1
+    return false if (x2 - x1).abs === 1 && (y2 - y1).abs === 2
+    return false if (y2 - y1).abs > 2
+    if @is_white
+      return false if y2 <= y1
+      if y2 - y1 === 2
+        return true if y1 === 1
+        return false
+      end
+    else
+      return false if y2 >= y1
+      if y2 - y1 === -2
+        return true if y1 === 6
+        return false
+      end
+    end
+    return true
   end
 end
 
 class King < Piece
   def initialize(is_white = true)
     super
-    is_white ? @img = "♔" : @img = "♚"
+    @is_white ? @img = "♚" : @img = "♔"
+  end
+
+  def correct_move?(x1, y1, x2, y2)
+    return false if (x2 - x1).abs > 1
+    return false if (y2 - y1).abs > 1
+    return true
   end
 end
 
 class Queen < Piece
   def initialize(is_white = true)
     super
-    is_white ? @img = "♕" : @img = "♛"
+    @is_white ? @img = "♛" : @img = "♕"
+  end
+
+  def correct_move?(x1, y1, x2, y2)
+
   end
 end
 
 class Bishop < Piece
   def initialize(is_white = true)
     super
-    is_white ? @img = "♗" : @img = "♝"
+    @is_white ? @img = "♝" : @img = "♗"
+  end
+
+  def correct_move?(x1, y1, x2, y2)
+
   end
 end
 
 class Knight < Piece
   def initialize(is_white = true)
     super
-    is_white ? @img = "♘" : @img = "♞"
+    @is_white ? @img = "♞" : @img = "♘"
+  end
+
+  def correct_move?(x1, y1, x2, y2)
+
   end
 end
 
 class Rook < Piece
   def initialize(is_white = true)
     super
-    is_white ? @img = "♖" : @img = "♜"
+    @is_white ? @img = "♜" : @img = "♖"
+  end
+
+  def correct_move?(x1, y1, x2, y2)
+
   end
 end
